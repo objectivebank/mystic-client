df2895f4901963bf04ba4bdca49282fdcdb4ec9e
Stephen Karger <stephen.karger@gmail.com>
2019-10-20 22:33:55 -0400

    Dynamically add ClipboardJS initialization script

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ClipboardJS from "clipboard";
 import { Elm } from "./Main.elm";
 
 window.Elm = Elm;
+
+const clipboardScript = document.createElement("script");
 window.ClipboardJS = ClipboardJS;
+const inlineScript = document.createTextNode("new ClipboardJS('#copy-button');");
+clipboardScript.appendChild(inlineScript);
+document.body.appendChild(clipboardScript)
+
